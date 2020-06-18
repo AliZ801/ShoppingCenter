e955dc2988b193d925d35d329afd8e07f7f617d1
Ali Zubair <ali.zubair98@outlook.com>
2020-06-18 18:08:46 +0300

    Comit 13

--- ShoppingCenter/Areas/Customer/Controllers/Boy.cs
+++ ShoppingCenter/Areas/Customer/Controllers/Boy.cs
@@ -9,14 +9,14 @@ using ShoppingCenter.Models.ViewModels;
 namespace ShoppingCenter.Areas.Customer.Controllers
 {
     [Area("Customer")]
-    public class MenController : Controller
+    public class Boy : Controller
     {
-        public readonly IUnitofWork _unitofWork;
+        private readonly IUnitofWork _unitofWork;
 
         [BindProperty]
         public CategoryVM categoryVM { get; set; }
 
-        public MenController(IUnitofWork unitofWork)
+        public Boy(IUnitofWork unitofWork)
         {
             _unitofWork = unitofWork;
         }
@@ -25,20 +25,20 @@ namespace ShoppingCenter.Areas.Customer.Controllers
         {
             categoryVM = new CategoryVM()
             {
-                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,ProductType,ProductSize", filter: p => p.Category.CatName == "Men")
+                ProductsList = _unitofWork.Products.GetAll(includeProperties: "ProductType,ProductSize", filter: p => p.Category.CatName == "Boy")
             };
 
             return View(categoryVM);
         }
 
-        public IActionResult Detail(int id)
+        public IActionResult Detail(int? id)
         {
             int count = 5;
 
             categoryVM = new CategoryVM()
             {
-                Products = _unitofWork.Products.GetFirstOrDefault(includeProperties: "ProductType,ProductSize", filter: p => p.Id == id),
-                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,ProductType,ProductSize").Take(count).ToList()
+                Products = _unitofWork.Products.Get(id.GetValueOrDefault()),
+                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,PropertyType,PropertySize", filter: p => p.Category.CatName == "Boy").Take(count).ToList()
             };
 
             return View(categoryVM);
@@ -48,7 +48,7 @@ namespace ShoppingCenter.Areas.Customer.Controllers
         {
             categoryVM = new CategoryVM()
             {
-                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,ProductType,ProductSize", filter: p => p.ProductType.Type == "T-Shirts" && p.Category.CatName == "Men")
+                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,PropertySize,PropertyType", filter: p => p.ProductType.Type == "T-Shirt" && p.Category.CatName == "Boy")
             };
 
             return View(categoryVM);
@@ -58,7 +58,7 @@ namespace ShoppingCenter.Areas.Customer.Controllers
         {
             categoryVM = new CategoryVM()
             {
-                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,ProductType,ProductSize", filter: p => p.ProductType.Type == "Shirts" && p.Category.CatName == "Men")
+                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,PropertySize,PropertyType", filter: p => p.ProductType.Type == "Shirt" && p.Category.CatName == "Boy")
             };
 
             return View(categoryVM);
@@ -68,7 +68,7 @@ namespace ShoppingCenter.Areas.Customer.Controllers
         {
             categoryVM = new CategoryVM()
             {
-                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,ProductType,ProductSize", filter: p => p.ProductType.Type == "Pants" && p.Category.CatName == "Men")
+                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,PropertySize,PropertyType", filter: p => p.ProductType.Type == "Pants" && p.Category.CatName == "Boy")
             };
 
             return View(categoryVM);
@@ -78,7 +78,7 @@ namespace ShoppingCenter.Areas.Customer.Controllers
         {
             categoryVM = new CategoryVM()
             {
-                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,ProductType,ProductSize", filter: p => p.ProductType.Type == "Shoes" && p.Category.CatName == "Men")
+                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,PropertySize,PropertyType", filter: p => p.ProductType.Type == "Shoes" && p.Category.CatName == "Boy")
             };
 
             return View(categoryVM);
@@ -88,7 +88,7 @@ namespace ShoppingCenter.Areas.Customer.Controllers
         {
             categoryVM = new CategoryVM()
             {
-                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,ProductType,ProductSize", filter: p => p.ProductType.Type == "Assessories" && p.Category.CatName == "Men")
+                ProductsList = _unitofWork.Products.GetAll(includeProperties: "Category,PropertySize,PropertyType", filter: p => p.ProductType.Type == "Assessories" && p.Category.CatName == "Boy")
             };
 
             return View(categoryVM);
